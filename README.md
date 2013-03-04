7aed9e2506e9b40b3ac2bca71709181ca2d9561d
Kyle Finley <kyle.finley@gmail.com>
2013-03-04 15:08:46 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Meteor Belt Flash
+
+Provides client side flash notifications
+
+## Installation
+
+Meteor Router can be installed with [Meteorite](https://github.com/oortcloud/meteorite/). From inside a Meteorite-managed app:
+
+``` sh
+$ mrt add belt-flash
+```
+
+## API
+
+```
+Belt.MD5.hash('test@example.com');
+```
+
+### Basics
---
+++ package.js
@@ -0,0 +1,17 @@
+Package.describe({
+  summary: "md5 for Meteor Belt"
+});
+
+Package.on_use(function (api, where) {
+  api.add_files([
+    'crypto-js/core.js',
+    'crypto-js/md5.js',
+    'md5_common.js'], ['client', 'server']);
+});
+
+Package.on_test(function (api) {
+  api.use('belt-md5');
+  api.use('tinytest');
+
+  api.add_files('md5_tests.js', 'server');
+});
